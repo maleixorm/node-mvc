1997abc185bc6ad2211e8bb6432c99e50f4ee8d7
Marcos Aleixo <maknytz@gmail.com>
2025-11-22 11:08:15 -0300

    Criando o arquivo de conexão com o banco de dados

---
+++ Db/conn.js
@@ -0,0 +1,15 @@
+const { Sequelize } = require('sequelize');
+
+const sequelize = new Sequelize('nodemvc', 'php', '123456', {
+    host: 'localhost',
+    dialect: 'mysql'
+});
+
+try {
+    sequelize.authenticate();
+    console.log("Conectamos ao banco de dados!");
+} catch (error) {
+    console.log(`Não foi possível conectar ao banco de dados: ${error}`);
+}
+
+exports.default = sequelize
