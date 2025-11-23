0999c9d42b431910ad3698ab0f258e17054a2268
Marcos Aleixo <maknytz@gmail.com>
2025-11-23 20:52:16 -0300

    Criando uma task para  o projeto

---
+++ Models/Task.js
@@ -0,0 +1,17 @@
+const {DataTypes} = require('sequelize')
+const db = require('../Db/conn')
+
+const Task = db.define('Task', {
+    title: {
+        type: DataTypes.STRING,
+        required: true
+    },
+    description: {
+        type: DataTypes.STRING,
+        required: true
+    },
+    done: {
+        type: DataTypes.BOOLEAN,
+        required: true
+    }
+})
