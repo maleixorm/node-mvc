3d1b8bef86d30f176b3fe570c8f5072d886394e4
Marcos Aleixo <maknytz@gmail.com>
2025-11-24 15:25:49 -0300

    Criando o método showTasks

--- Controllers/TaskController.js
+++ Controllers/TaskController.js
@@ -4,4 +4,7 @@ module.exports = class TaskController {
     static createTask(req, res) {
         res.render('tasks/create')
     }
+    static showTasks(req, res) {
+        res.render('tasks/all')
+    }
 }
